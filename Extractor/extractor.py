dd736af6464ae9c19b28c53ff47cd14b9f6e9e9f
Bhoomika-stride <139863215+Bhoomika-stride@users.noreply.github.com>
2023-09-28 09:18:07 +0000

    made changes to bullet and num to restore prper structure

--- Extractor/extractor.py
+++ Extractor/extractor.py
@@ -1,12 +1,7 @@
-import fitz
-
-from Extractor.helpers import drop_tables, get_blocks,get_paras_headings
-from Extractor.helpers import get_bullets, get_numbered_list, postprocessing, get_json
-
 def extract_para_head_bullet_num(doc_path):
   doc_path = doc_path
   doc = fitz.open(doc_path)
-
+  no_of_pages = len(doc)
   blocks, avg_font_size = get_blocks(doc)
   non_table_blocks = drop_tables(blocks)
   para_heading_detections = get_paras_headings(non_table_blocks,avg_font_size)
@@ -16,14 +11,15 @@ def extract_para_head_bullet_num(doc_path):
   bullets = get_bullets(blocks)
   numbered = get_numbered_list(blocks)
 
-  # final = postprocessing(paragraphs, headings, bullets, numbered, len(doc))
-  # paragraphs = final[0]
-  # headings = final[1]
+  combined_bullets = combine_structurally(bullets, no_of_pages)
+  combined_numbered = combine_structurally(numbered, no_of_pages)
 
-  # bullets = final[2]
-  # numbered = final[3]
+  final = postprocessing(paragraphs, headings, combined_bullets, combined_numbered, no_of_pages)
+  paragraphs = final[0]
+  headings = final[1]
+
+  bullets = final[2]
+  numbered = final[3]
   final_blocks = get_json(headings, paragraphs, bullets, numbered)
 
   return final_blocks
-
-
