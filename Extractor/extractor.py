ddc989f7258beb913e7139e63b37439f2585a900
Bhoomika-stride <139863215+Bhoomika-stride@users.noreply.github.com>
2023-09-29 08:19:49 +0000

    Fixed Flase Positives in bullets and numbered list

--- Extractor/extractor.py
+++ Extractor/extractor.py
@@ -2,29 +2,45 @@ import fitz
 
 from Extractor.helpers import drop_tables, get_blocks,get_paras_headings
 from Extractor.helpers import get_bullets, get_numbered_list, postprocessing, get_json, combine_structurally
-
 def extract_para_head_bullet_num(doc_path):
-  doc_path = doc_path
-  doc = fitz.open(doc_path)
-  no_of_pages = len(doc)
-  blocks, avg_font_size = get_blocks(doc)
-  non_table_blocks = drop_tables(blocks)
-  para_heading_detections = get_paras_headings(non_table_blocks,avg_font_size)
-  paragraphs = para_heading_detections[0]
-  headings = para_heading_detections[1]
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+  final_blocks = {
+              "heading" : [],
+              "paragraph" : [],
+              "bullet_list" :[],
+              "numbered_list" : []
+              }
+  try:
+    doc_path = doc_path
+    doc = fitz.open(doc_path)
+    no_of_pages = len(doc)
+    blocks, avg_font_size = get_blocks(doc)
+    non_table_blocks = drop_tables(blocks)
+    para_heading_detections = get_paras_headings(non_table_blocks,avg_font_size)
+    paragraphs = para_heading_detections[0]
+    headings = para_heading_detections[1]
 
-  bullets = get_bullets(blocks)
-  numbered = get_numbered_list(blocks)
+    bullets = get_bullets(blocks)
+    numbered = get_numbered_list(blocks)
+    # print(bullets)
+    combined_bullets = combine_structurally(bullets, no_of_pages)
+    # print(combined_bullets)
+    combined_numbered = combine_structurally(numbered, no_of_pages)
 
-  combined_bullets = combine_structurally(bullets, no_of_pages)
-  combined_numbered = combine_structurally(numbered, no_of_pages)
+    final = postprocessing(paragraphs, headings, combined_bullets, combined_numbered, no_of_pages)
+    paragraphs = final[0]
+    headings = final[1]
 
-  final = postprocessing(paragraphs, headings, combined_bullets, combined_numbered, no_of_pages)
-  paragraphs = final[0]
-  headings = final[1]
-
-  bullets = final[2]
-  numbered = final[3]
-  final_blocks = get_json(headings, paragraphs, bullets, numbered)
+    bullets = final[2]
+    # print(bullets)
+    numbered = final[3]
+    final_blocks = get_json(headings, paragraphs, bullets, numbered)
+  except:
+    pass
 
   return final_blocks
--- Extractor/helpers.py
+++ Extractor/helpers.py
@@ -2,46 +2,57 @@ import re
 import statistics
 import math
 import fitz
-import fitz
-import statistics
-import math
-import re
-import math
-
 def drop_tables(blocks):
+
+  '''
+  this function takes a block of text and based on dots i.e . and spacing between each words of the same line
+  checks if its a table or TOC and drops those blocks
+  Input : blocks - text block dataset created earlier
+  Output : new_blocks - text blocks without tables and TOC
+  '''
+
   new_blocks = []
   for block in blocks: #for each block
 
-    if not re.search("[.]{3,}", block[2]):
+    if not re.search("[.]{3,}", block[2]): #check if it doesnt have more than 3 dots cuz its mostly TOC
       avg_space = []
       ideal_space = 25
-      # print(block[7])
       if len(block[4]) == 1:
           new_blocks.append(block)
 
       for i in range(len(block[4]) - 1): # for each word in each block
         if math.floor(block[4][i][3]) == math.floor(block[4][i + 1][3]): #check if the consecutive words are in the same line.
-          avg_space.append(abs(block[4][i][2] - block[4][i + 1][0]))
+          avg_space.append(abs(block[4][i][2] - block[4][i + 1][0])) # if yes calclate the sapce betwween the words
 
       if len(avg_space) != 0:
         block_avg_space = sum(avg_space)/ len(avg_space)
 
         if block_avg_space <= ideal_space or (len(block[4]) > 1 and len(avg_space) == 0):
+          #if the avg space between words is less than ideal space then proceed
           new_blocks.append(block)
 
   return new_blocks
 
 def group_text_to_blocks(words):
 
+  '''
+  link : https://pymupdf.readthedocs.io/en/latest/app1.html
+  this function takes individual words present in a page
+  and groups them based on the block number hence getting individual words present in a block
+  Input : words - tuple of individual words present in a page format (x0, y0, x1, y1, "word", block_no, line_no, word_no)
+  Output : list_of_lists - list of lists where each list is list of tuples of each word present within a block which is the index no 
+  where each tuple follows (x0, y0, x1, y1, "word", block_no, line_no, word_no) format
+
+  '''
+
   # Create a dictionary to group tuples by their second element
   grouped_dict = {}
 
   for tup in words:
-      key = tup[5]  # Use the second element as the key
+      key = tup[5]  # Use the block number to 
       if key in grouped_dict:
           grouped_dict[key].append(tup)
       else:
-          # print(key)
           grouped_dict[key] = [tup]
 
   # Convert the dictionary values to a list of lists
@@ -50,6 +61,13 @@ def group_text_to_blocks(words):
   return list_of_lists
 
 def get_font_info(lines):
+
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   block_info = {}
   overall_font_size = []
   for block in lines:
@@ -72,11 +90,19 @@ def get_font_info(lines):
       overall_font_size.append(font_size)
       block_info[block["number"]] = [font_type, font_size, lines_]
   final = [block_info, overall_font_size]
+
   return final
 
 
 
 def get_blocks(doc):
+
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   blocks = []
   texts = []
   avg_font_size_ = []
@@ -115,10 +141,21 @@ def get_blocks(doc):
 
       #new_block format ( 0 bbox, 1 page_no, 2 text, 3 block_no, 4 individual words,5 font type+style, 6 font size, 7 Lines, 8( page width, page height))
       blocks.extend(new_block)
-  avg_font_size = statistics.mean(avg_font_size_)
+  try:
+    avg_font_size = statistics.mean(avg_font_size_)
+  except:
+    avg_font_size = 20
+    print("no data present")
   return blocks, avg_font_size
 
 def get_block_bbox(word_bboxes):
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   # print(word_bboxes)
   # Initialize variables to store min and max coordinates
   min_x, min_y = float('inf'), float('inf')
@@ -133,17 +170,23 @@ def get_block_bbox(word_bboxes):
   return (min_x, min_y, max_x, max_y)
 
 def get_paras_headings(blocks, avg_font_size):
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
 
   para = []
   heading = []
   for block in blocks: #for each block
 
       # print(block[2], block[2].isupper(),len(block[4]) > 5,block[5], block[2])
-      if  not block[2].isupper() and len(block[4]) > 7 and not re.search("Bold", block[5]) and not re.search("^[•●○*◦▪▫■◆◘‣⁌❖⮞⬑➔⇒➢✓-]", block[2]):
+      if  not block[2].isupper() and len(block[4]) > 7 and not re.search("Bold", block[5]) and not re.search("^[•●○*◦▪▫■◆◘‣⁌❖⮞⬑➔⇒➢✓-]", block[2]) and block[6]<=avg_font_size+1 and re.search("([a-zA-Z]+)", block[2]):
         # check if the block doesnot contains text which is all upper and no of words in the block > 10 and if the text is not bold
         para.append(block)
 
-      elif  (block[2].isupper() and block[6] > avg_font_size) or block[6] > avg_font_size  or re.search("Bold", block[5]) :
+      elif  ((block[2].isupper() and block[6] > avg_font_size) or block[6] >= avg_font_size + 2  or re.search("Bold", block[5]))  and not re.search("^[•●○*◦▪▫■◆◘‣⁌❖⮞⬑➔⇒➢✓-]", block[2]) and re.search("([a-zA-Z]+)", block[2]):
         # check if the block doesnot contains text which is all upper and no of words in the block > 10 and if the text is not bold
         heading.append(block)
   final = [para, heading]
@@ -151,8 +194,15 @@ def get_paras_headings(blocks, avg_font_size):
 
 def get_bullets(blocks):
 
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   lines =  []
   new_blocks = []
+  text_patt = '([a-zA-Z]+)'
   for block in blocks:
     # print(block[1], block[2])
     page_no = block[1]
@@ -161,6 +211,7 @@ def get_bullets(blocks):
       b["page_no"] = page_no
       b["page_size"] = page_size
       lines.append(b)
+      # print(b)
   # print(lines)
 
   text = ''
@@ -169,8 +220,9 @@ def get_bullets(blocks):
     line_no+=1
     if not line["text"].isspace():
       if re.search("^[•●○*◦▪▫■◆◘‣⁌❖⮞⬑➔⇒➢✓-]", line["text"]):
-
-        if text!='':
+        # print("1", line["text"])
+        if len(text) >=3 and re.search(text_patt, text):
+          # print("2", text)
           new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
 
         text = ''
@@ -186,28 +238,39 @@ def get_bullets(blocks):
         # print(line["origin"], line["text"])
 
       elif text!='':
-        # print(line["origin"], o[0], math.floor(o[0]) - 20<= math.floor(line["origin"][0]), math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1]),math.floor(o[0]) + 20>= math.floor(line["origin"][0]), line["text"])
-        if line["page_no"] == page_no and ((math.floor(o[1]) == math.floor(line["origin"][1]))or ((math.floor(o[0]) + 10<= math.floor(line["origin"][0]))and (math.floor(o[0]) + 20>= math.floor(line["origin"][0])) and ( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1])))):
-
+        # print(line["origin"], o, (abs((o[1]) - line["origin"][1]) <=3),(math.floor(o[0]) + 10<= math.floor(line["origin"][0])),(math.floor(o[0]) + 20>= math.floor(line["origin"][0])),( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1])),line["page_no"] == page_no and ((abs((o[1]) - line["origin"][1]) <=3) or ((math.floor(o[0]) + 10<= math.floor(line["origin"][0])) and (math.floor(o[0]) + 20>= math.floor(line["origin"][0])) and ( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1])))), line["text"])
+        if line["page_no"] == page_no and ((abs((o[1]) - line["origin"][1]) <=3) or ((math.floor(o[0]) + 10<= math.floor(line["origin"][0])) and (math.floor(o[0]) + 20>= math.floor(line["origin"][0])) and ( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1]))) or (abs((origin[-1][1]) - line["origin"][1]) <=3)):
+# check if the two lines are in same page and the new lines y origin matches with the previos lines y origin
+# or if the new line is indented within a range of the previous line both in x and y direction
+#or if it aligns in y direction with the last appended word
             origin.append(line["origin"])
             # o = origin[1]
             text+= ' ' + line["text"]
             bbox.append(line["bbox"])
 
-        else:
+        elif len(text) >=3 and re.search(text_patt, text):
+          # print("3", text)
           new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
           text = ''
           bbox = []
           origin = []
-      if line_no == len(lines) and text!='':
+      if line_no == len(lines) and len(text) >=3 and re.search(text_patt, text):
+        # print("4", text)
         new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
 
   return new_blocks
 
 def get_numbered_list(blocks):
 
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   lines =  []
   new_blocks = []
+  text_patt = "([a-zA-Z]+)"
   for block in blocks:
     # print(block[1], block[2])
     page_no = block[1]
@@ -223,8 +286,8 @@ def get_numbered_list(blocks):
     # print(line)
     line_no+=1
     if not line["text"].isspace():
-      if re.search("^(((M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))&(\.|\ ))|([0-9]{1,2}\.|[a-zA-Z]\.\ |\(\ ?[a-zA-Z]\ ?\))|((m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?X{0,3})(ix|iv|v?i{0,3}))&(\.|\ ))|[0-9])", line["text"]) :
-        if text!='':
+      if re.search("^(((M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))&(\.|\ ))|([0-9]{1,2}\.|[a-zA-Z]\.\ |\(\ ?[a-zA-Z]\ ?\))|((m{0,4}(cm|cd|d?c{0,3})(xc|xl|l?X{0,3})(ix|iv|v?i{0,3}))&(\.|\ ))|[0-9]{1,2})", line["text"]) :
+        if len(text) >=3 and re.search(text_patt, text):
           new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
 
         text = ''
@@ -239,26 +302,33 @@ def get_numbered_list(blocks):
         o = origin[0]
 
       elif text!='':
-        if line["page_no"] == page_no and ((math.floor(o[1]) == math.floor(line["origin"][1]))or ((math.floor(o[0]) + 10<= math.floor(line["origin"][0]))and (math.floor(o[0]) + 20>= math.floor(line["origin"][0])) and ( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1])))):
+        if line["page_no"] == page_no and ((abs((o[1]) - line["origin"][1]) <=3) or ((math.floor(o[0]) + 10<= math.floor(line["origin"][0])) and (math.floor(o[0]) + 20>= math.floor(line["origin"][0])) and ( math.floor(origin[-1][1]) + 20>= math.floor(line["origin"][1]))) or (abs((origin[-1][1]) - line["origin"][1]) <=3)):
 # check if the two lines are in same page and the new lines y origin matches with the previos lines y origin
-# or if the new line is indented within a range of the previous line
+# or if the new line is indented within a range of the previous line both in x and y direction
+#or if it aligns in y direction with the last appended word
             origin.append(line["origin"])
             # o = origin[1]
             text+= ' ' + line["text"]
             bbox.append(line["bbox"])
 
-        else:
+        elif len(text) >=3 and re.search(text_patt, text):
           new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
           text = ''
           bbox = []
           origin = []
-      if line_no == len(lines) and text!='':
+      if line_no == len(lines) and len(text) >=3 and re.search(text_patt, text):
         new_blocks.append([get_block_bbox(bbox), page_no,text, line["page_size"]])
 
   return new_blocks
 
 
 def cal_area(bbox):
+
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
 
   width = abs(bbox[2]- bbox[0])
   height = abs(bbox[3]- bbox[1])
@@ -267,38 +337,60 @@ def cal_area(bbox):
   return area
 
 def filter_overlap(list_one, list_two):
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   result = [item for item in list_one if item not in list_two]
   return result
 
 def do_bboxes_overlap(box1, box2):
-    # Extract coordinates of the first box
-    x1a, y1a, x2a, y2a = box1
-    # Extract coordinates of the second box
-    x1b, y1b, x2b, y2b = box2
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
 
-    # Calculate the area of the first box
-    area1 = (x2a - x1a) * (y2a - y1a)
-    # Calculate the area of the second box
-    area2 = (x2b - x1b) * (y2b - y1b)
+  # Extract coordinates of the first box
+  x1a, y1a, x2a, y2a = box1
+  # Extract coordinates of the second box
+  x1b, y1b, x2b, y2b = box2
 
-    # Calculate the coordinates of the intersection rectangle
-    x_intersection = max(0, min(x2a, x2b) - max(x1a, x1b))
-    y_intersection = max(0, min(y2a, y2b) - max(y1a, y1b))
+  # Calculate the area of the first box
+  area1 = (x2a - x1a) * (y2a - y1a)
+  # Calculate the area of the second box
+  area2 = (x2b - x1b) * (y2b - y1b)
 
-    # Calculate the area of the intersection rectangle
-    intersection_area = x_intersection * y_intersection
+  # Calculate the coordinates of the intersection rectangle
+  x_intersection = max(0, min(x2a, x2b) - max(x1a, x1b))
+  y_intersection = max(0, min(y2a, y2b) - max(y1a, y1b))
 
-    # Calculate the percentage of overlap
-    overlap_percentage = (intersection_area / min(area1, area2)) * 100
+  # Calculate the area of the intersection rectangle
+  intersection_area = x_intersection * y_intersection
 
-    # Return True if overlap percentage is above 75%, otherwise return False
-    return overlap_percentage > 75
+  # Calculate the percentage of overlap
+  overlap_percentage = (intersection_area / min(area1, area2)) * 100
+
+  # Return True if overlap percentage is above 75%, otherwise return False
+  return overlap_percentage > 75
 
 
 def remove_para_headings(paragraphs, headings, target_list):
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
+
   non_para = []
   non_head = []
   non_target = []
+  # print(target_list)
 
   for each_block in target_list:
     for each_para in paragraphs:
@@ -309,7 +401,7 @@ def remove_para_headings(paragraphs, headings, target_list):
         diff = para_area - bullet_area
 
         if diff <= 100:
-          #if difference in area is too much then its 
+          #if difference in area is too much then its
           non_para.append(each_para)
           # print("np", each_para[2])
         else:
@@ -326,7 +418,7 @@ def remove_para_headings(paragraphs, headings, target_list):
         # diff = para_area - bullet_area
         # if abs(diff) > = 100:
         non_target.append(each_block)
-          # print("nbh",diff, each_block[2], each_head[2])
+        # print("nbh",diff, each_block[2], each_head[2])
         # else:
         # non_head.append(each_head)
           # print("nh",diff, each_block[2], each_head[2])
@@ -334,11 +426,16 @@ def remove_para_headings(paragraphs, headings, target_list):
   para = filter_overlap(paragraphs, non_para)
   head = filter_overlap(headings, non_head)
   target = filter_overlap(target_list, non_target)
-  # print(head)
 
   return [para, head, target]
 
 def postprocessing(paragraphs, headings, bullets, numbered, no_of_pages):
+
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
 
   paragraphs_new = []
   headings_new = []
@@ -362,17 +459,27 @@ def postprocessing(paragraphs, headings, bullets, numbered, no_of_pages):
   return final
 
 def combine_structurally(target, no_of_pages):
+  
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
 
-  combined_target = [[target[0]]]
-  i = 0
-  for each_line in target[1:]:
-    if math.floor(each_line[0][0]) == math.floor(combined_target[-1][-1][0][0]) and each_line[1] == combined_target[-1][-1][1] and (each_line[0][1] - combined_target[-1][-1][0][3]) <=5:
-      #check if two blocks are in same page and if they have the same x0 and if the difference between
-      # the y1 of first block and y0 of second block is below some threshold
-      combined_target[i].append(each_line)
-    else:
-      i+=1
-      combined_target.append([each_line])
+  combined_target = []
+  try:
+    combined_target = [[target[0]]]
+    i = 0
+    for each_line in target[1:]:
+      if math.floor(each_line[0][0]) == math.floor(combined_target[-1][-1][0][0]) and each_line[1] == combined_target[-1][-1][1] and (each_line[0][1] - combined_target[-1][-1][0][3]) <=5:
+        #check if two blocks are in same page and if they have the same x0 and if the difference between
+        # the y1 of first block and y0 of second block is below some threshold
+        combined_target[i].append(each_line)
+      else:
+        i+=1
+        combined_target.append([each_line])
+  except:
+    print("No data present")
 
   new_block = []
   for each_line in combined_target:
@@ -388,58 +495,72 @@ def combine_structurally(target, no_of_pages):
 
 def get_json(headings, paras, bullets, nums):
 
-  para = []
-  for x in paras:
-    para_json = {
-                "bbox" : x[0],
-                "text" : x[2],
-                "page_no" : x[1],
-                "font_type" : x[5],
-                "font_size" : x[6],
-                "page_size" : x[8]
-
-    }
-    para.append(para_json)
-
-  heading = []
-  for x in headings:
-    heading_json = {
-                "bbox" : x[0],
-                "text" : x[2],
-                "page_no" : x[1],
-                "font_type" : x[5],
-                "font_size" : x[6],
-                "page_size" : x[8]
-
-    }
-    heading.append(heading_json)
-
-  bullet = []
-  for x in bullets:
-    bullet_json = {
-                "bbox" : x[0],
-                "text" : x[2],
-                "page_no" : x[1],
-                "page_size" : x[3]
-    }
-    bullet.append(bullet_json)
-
-  num = []
-  for x in nums:
-    # print(x[1], x[2])
-    num_json = {
-                "bbox" : x[0],
-                "text" : x[2],
-                "page_no" : x[1],
-                "page_size" : x[3]
-    }
-    num.append(num_json)
-
+  '''
+  this function takes 
+  Input : 
+  Output : 
+  '''
   final_list = {
-                "heading" : heading,
-                "paragraph" : para,
-                "bullet_list" :bullet,
-                "numbered_list" : num
-                }
+              "heading" : [],
+              "paragraph" : [],
+              "bullet_list" :[],
+              "numbered_list" : []
+              }
+  try:          
+    para = []
+    for x in paras:
+      para_json = {
+                  "bbox" : x[0],
+                  "text" : x[2],
+                  "page_no" : x[1],
+                  "font_type" : x[5],
+                  "font_size" : x[6],
+                  "page_size" : x[8]
+
+      }
+      para.append(para_json)
+
+    heading = []
+    for x in headings:
+      heading_json = {
+                  "bbox" : x[0],
+                  "text" : x[2],
+                  "page_no" : x[1],
+                  "font_type" : x[5],
+                  "font_size" : x[6],
+                  "page_size" : x[8]
+
+      }
+      heading.append(heading_json)
+
+    bullet = []
+    for x in bullets:
+      bullet_json = {
+                  "bbox" : x[0],
+                  "text" : x[2],
+                  "page_no" : x[1],
+                  "page_size" : x[3]
+      }
+      bullet.append(bullet_json)
+
+    num = []
+    for x in nums:
+      # print(x[1], x[2])
+      num_json = {
+                  "bbox" : x[0],
+                  "text" : x[2],
+                  "page_no" : x[1],
+                  "page_size" : x[3]
+      }
+      num.append(num_json)
+
+    final_list = {
+                  "heading" : heading,
+                  "paragraph" : para,
+                  "bullet_list" :bullet,
+                  "numbered_list" : num
+                  }
+  except Exception as e:
+    print("Exception while cretaing final json", e)
 
   return final_list
